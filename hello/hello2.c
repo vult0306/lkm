cf3af0dcdb3902739555db9194fede41d2b2d94f
vult0306 <letuanvu91@gmail.com>
2024-04-30 00:42:19 +0700

    add hello2

--- hello/hello2.c
+++ hello/hello2.c
@@ -1,18 +1,21 @@
 #include <linux/module.h>   /* needed for all modules */
 #include <linux/kernel.h>   /* needed for kern_info */
+#include <linux/init.h>     /* needed for the macros */
 
 #define DRIVER_AUTHOR "vle <letuanvu91@gmail.com>"
 #define DRIVER_DESC "A sample hello world driver"
 
-int init_module(void) {
-    printk(KERN_INFO "hello world \n");
+static int __init hello2_init(void) {
+    printk(KERN_INFO "hello world 2 \n");
     return 0;
 }
 
-void cleanup_module(void) {
-    printk(KERN_INFO "goodbye world\n");
+static void __exit hello2_exit(void) {
+    printk(KERN_INFO "goodbye world 2\n");
 }
 
+module_init(hello2_init);
+module_exit(hello2_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
